0fb0b0e38084f99e329a7e769cfc61c01896c818
jskulthe <jskulthe@gmail.com>
2019-11-18 22:24:41 +0530

    First commit

--- src/main/java/com/example/trafficLight/controller/TrafficController.java
+++ src/main/java/com/example/trafficLight/controller/TrafficController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TrafficController {
 
-	@GetMapping("/getSignalStatus")
+	//@GetMapping("/getSignalStatus")
 	
 }
